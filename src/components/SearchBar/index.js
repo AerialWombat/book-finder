003151cc017e55926647e08855e23b2014a20f5e
AerialWombat <adrianzhan@gmail.com>
2019-02-26 20:43:02 -0500

    Added stylers for search form

--- src/components/SearchBar/index.js
+++ src/components/SearchBar/index.js
@@ -1,10 +1,15 @@
 import React from "react";
+import styles from "./SearchBar.module.scss";
 
 const SearchBar = () => {
   return (
-    <form>
-      <input type="text" placeholder="book title..." />
-      <input type="submit" value="Search" />
+    <form className={styles.searchForm}>
+      <input
+        className={styles.textInput}
+        type="text"
+        placeholder="book title..."
+      />
+      <input className={styles.submitBtn} type="submit" value="Search" />
     </form>
   );
 };
