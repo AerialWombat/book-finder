6bbad3027fa59bbc769c13bfa5aed7e41b54a224
AerialWombat <adrianzhan@gmail.com>
2019-03-02 15:24:10 -0500

    Fixed some edge cases breaking card layout

--- src/components/BookCard/index.js
+++ src/components/BookCard/index.js
@@ -4,19 +4,35 @@ import styles from "./BookCard.module.scss";
 const BookCard = ({ volumeInfo }) => {
   return (
     <li className={styles.container}>
-      <img
-        className={styles.thumbnail}
-        src={volumeInfo.imageLinks.smallThumbnail}
-        alt="Cover art"
-      />
-      <h1 className={styles.title}>{volumeInfo.title}</h1>
+      {volumeInfo.imageLinks ? (
+        <img
+          className={styles.thumbnail}
+          src={volumeInfo.imageLinks.smallThumbnail}
+          alt="Cover art"
+        />
+      ) : (
+        <p className={styles.missingThumbnail}>Missing Image</p>
+      )}
+      <h1 className={styles.title}>
+        {volumeInfo.title ? volumeInfo.title : "Missing title data"}
+      </h1>
       <div>
-        <h2 className={styles.subtitle}>Author</h2>
-        <p>{volumeInfo.authors[0]}</p>
+        <h2 className={styles.subtitle}>Author(s)</h2>
+        <div>
+          {volumeInfo.authors ? (
+            volumeInfo.authors.map(author => <p>{author}</p>)
+          ) : (
+            <p>Missing author data</p>
+          )}
+        </div>
       </div>
       <div>
         <h2 className={styles.subtitle}>Publishing Company</h2>{" "}
-        <p>{volumeInfo.publisher}</p>
+        <p>
+          {volumeInfo.publisher
+            ? volumeInfo.publisher
+            : "Missing publisher data"}
+        </p>
       </div>
       <div className={styles.subtitle}>
         <a
