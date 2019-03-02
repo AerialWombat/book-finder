254bac1df6f1059330b8afd8e26e13c4762630a1
AerialWombat <adrianzhan@gmail.com>
2019-03-02 15:08:43 -0500

    Added responsive functionality to result viewer

--- src/components/BookCard/index.js
+++ src/components/BookCard/index.js
@@ -3,32 +3,30 @@ import styles from "./BookCard.module.scss";
 
 const BookCard = ({ volumeInfo }) => {
   return (
-    <li>
-      <article className={styles.container}>
-        <img
-          className={styles.thumbnail}
-          src={volumeInfo.imageLinks.smallThumbnail}
-          alt="Cover art"
-        />
-        <h1 className={styles.title}>{volumeInfo.title}</h1>
-        <div>
-          <h2 className={styles.subtitle}>Author</h2>
-          <p>{volumeInfo.authors[0]}</p>
-        </div>
-        <div>
-          <h2 className={styles.subtitle}>Publishing Company</h2>{" "}
-          <p>{volumeInfo.publisher}</p>
-        </div>
-        <div className={styles.subtitle}>
-          <a
-            target="_blank"
-            href={volumeInfo.canonicalVolumeLink}
-            rel="noopener noreferrer"
-          >
-            Learn More...
-          </a>
-        </div>
-      </article>
+    <li className={styles.container}>
+      <img
+        className={styles.thumbnail}
+        src={volumeInfo.imageLinks.smallThumbnail}
+        alt="Cover art"
+      />
+      <h1 className={styles.title}>{volumeInfo.title}</h1>
+      <div>
+        <h2 className={styles.subtitle}>Author</h2>
+        <p>{volumeInfo.authors[0]}</p>
+      </div>
+      <div>
+        <h2 className={styles.subtitle}>Publishing Company</h2>{" "}
+        <p>{volumeInfo.publisher}</p>
+      </div>
+      <div className={styles.subtitle}>
+        <a
+          target="_blank"
+          href={volumeInfo.canonicalVolumeLink}
+          rel="noopener noreferrer"
+        >
+          Learn More...
+        </a>
+      </div>
     </li>
   );
 };
