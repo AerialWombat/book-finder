d71a94cba87767e63f036b709cd879f8584c8abf
AerialWombat <adrianzhan@gmail.com>
2019-03-02 16:38:52 -0500

    Updates

---
+++ precache-manifest.ec5495104fdea5afa816d715a489d84f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "b078f3164b12622a0485570ad5733834",
+    "url": "/book-finder/static/media/spinner.b078f316.webm"
+  },
+  {
+    "revision": "736fd4229ce5946055cf",
+    "url": "/book-finder/static/js/runtime~main.736fd422.js"
+  },
+  {
+    "revision": "3456a5d6b66656e210de",
+    "url": "/book-finder/static/js/main.3456a5d6.chunk.js"
+  },
+  {
+    "revision": "e908f9e6b635e1a57f1b",
+    "url": "/book-finder/static/js/2.e908f9e6.chunk.js"
+  },
+  {
+    "revision": "3456a5d6b66656e210de",
+    "url": "/book-finder/static/css/main.0523e77d.chunk.css"
+  },
+  {
+    "revision": "cdba4719f07c21384b84a4e712896385",
+    "url": "/book-finder/index.html"
+  }
+];
