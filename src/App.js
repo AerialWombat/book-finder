7df283d29075a0c80619b786a0734883b88f1b5a
AerialWombat <adrianzhan@gmail.com>
2019-03-02 16:04:16 -0500

    Added conditional rendering logic for main content area

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ class App extends Component {
   state = {
     isFetching: false,
     searchQuery: null,
-    volumes: []
+    volumes: null
   };
 
   searchChange = event => {
@@ -20,40 +20,73 @@ class App extends Component {
   // Makes a GET request to Google Books API with the search query and then stores it in state
   fetchVolumes = event => {
     // Checks for blank query
-    if (this.state.searchQuery == null) {
+    if (!this.state.searchQuery) {
       event.preventDefault();
       return;
-    }
-    this.setState({ isFetching: true });
-    fetch(
-      `https://www.googleapis.com/books/v1/volumes?q=${
-        this.state.searchQuery
-      }&maxResults=20&key=${GBOOKS_API_KEY}`,
-      {
-        method: "GET",
-        mode: "cors"
-      }
-    )
-      .then(res =>
-        res.json().then(data =>
-          this.setState({
-            volumes: data.items
+    } else {
+      this.setState({ isFetching: true });
+      fetch(
+        `https://www.googleapis.com/books/v1/volumes?q=${
+          this.state.searchQuery
+        }&maxResults=20&key=${GBOOKS_API_KEY}`,
+        {
+          method: "GET",
+          mode: "cors"
+        }
+      )
+        .then(res =>
+          res.json().then(data => {
+            this.setState({
+              volumes: data.items
+            });
+            this.setState({ isFetching: false });
           })
         )
-      )
-      .catch(err => console.log(err));
-    event.preventDefault();
+        .catch(err => console.log(err));
+      event.preventDefault();
+    }
+  };
+
+  getContent = () => {
+    const { isFetching, volumes } = this.state;
+
+    let content;
+    if (isFetching) {
+      content = (
+        <main>
+          <video
+            className={styles.altContent}
+            src={require("./assets/spinner.webm")}
+            type="video/webm"
+            autoPlay
+            loop
+          />
+        </main>
+      );
+    } else if (!volumes && !isFetching) {
+      content = (
+        <main>
+          <h1 className={styles.altContent}>Search for books</h1>
+        </main>
+      );
+    } else {
+      content = (
+        <main>
+          <ResultViewer volumes={volumes} />
+        </main>
+      );
+    }
+    return content;
   };
 
   render() {
-    const { volumes } = this.state;
     return (
       <div className={styles.App}>
         <Header
           searchChange={this.searchChange}
           fetchVolumes={this.fetchVolumes}
         />
-        <ResultViewer volumes={volumes} />
+        {this.getContent()}
         <Footer />
       </div>
     );
