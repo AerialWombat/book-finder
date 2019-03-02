dd800f9644a22e98d06fb6ee9fa66b6688aafd3e
AerialWombat <adrianzhan@gmail.com>
2019-03-02 14:59:18 -0500

    Styled book cards

--- src/components/BookCard/index.js
+++ src/components/BookCard/index.js
@@ -3,21 +3,29 @@ import styles from "./BookCard.module.scss";
 
 const BookCard = ({ volumeInfo }) => {
   return (
-    <article>
+    <article className={styles.container}>
+      <img
+        className={styles.thumbnail}
+        src={volumeInfo.imageLinks.smallThumbnail}
+        alt="Cover art"
+      />
+      <h1 className={styles.title}>{volumeInfo.title}</h1>
       <div>
-        <img src={volumeInfo.imageLinks.smallThumbnail} alt="Cover art" />
-        <p>Title: {volumeInfo.title}</p>
-        <p>Author: {volumeInfo.authors[0]}</p>
-        <p>Publishing Company: {volumeInfo.publisher}</p>
-        <p>
-          <a
-            target="_blank"
-            href={volumeInfo.canonicalVolumeLink}
-            rel="noopener noreferrer"
-          >
-            Learn More
-          </a>
-        </p>
+        <h2 className={styles.subtitle}>Author</h2>
+        <p>{volumeInfo.authors[0]}</p>
+      </div>
+      <div>
+        <h2 className={styles.subtitle}>Publishing Company</h2>{" "}
+        <p>{volumeInfo.publisher}</p>
+      </div>
+      <div className={styles.subtitle}>
+        <a
+          target="_blank"
+          href={volumeInfo.canonicalVolumeLink}
+          rel="noopener noreferrer"
+        >
+          Learn More...
+        </a>
       </div>
     </article>
   );
